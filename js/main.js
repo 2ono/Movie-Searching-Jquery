a6ad5a223a0dffd10ca1976603f28b15726dfbf7
2ono <clt3555@gmail.com>
2023-05-03 12:47:57 +0900

    final commit

--- js/main.js
+++ js/main.js
@@ -7,24 +7,81 @@ $(document).ready(() => {
 })
 
 function getMovies(searchText) {
-    // 하루 천개 사용할수 있는 개인 api입니다. 따로 사용하실분은 가입 따로 하시고 api받으세요.
     axios.get('http://www.omdbapi.com/?apikey=340ad3d5&s=' + searchText)
         .then((response) => {
             console.log(response);
             let movies = response.data.Search;
-            let optput = '';
+            let output = '';
             $.each(movies, (index, movie) => {
                 output += `
                     <div class="col-md-3">
                      <div class="well text-center">
                       <img src="${movie.Poster}">
                       <h5>${movie.Title}</h5>
+                      <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
                     </div>
                 </div>
                 `;
-            })
+            });
+
+            $('#movies').html(output);
         })
         .catch((err) => {
             console.log(err);
-        })
+        });
+
+
+}
+
+
+function movieSelected(id) {
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
+
+function getMovie() {
+    let movieId = sessionStorage.getItem('movieId');
+
+    axios.get('http://www.omdbapi.com/?apikey=340ad3d5&i=' + movieId)
+    .then((response) => {
+        console.log(response);
+        let movie = response.data;
+        
+        let output = `
+         <div class="row">
+          <div class="col-md-4">
+            <img src="${movie.Poster}" class="thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h2>${movie.Title}</h2>
+            <ul class="list-group">
+                <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+                <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+               
+            </ul>
+          </div>
+         </div>
+         <div class="row">
+            <div class="well">
+                <h3>Plot</h3>
+                ${movie.Plot}
+                <hr>
+                <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+                <a href="index.html" class="btn btn-default">Go Back To Search 무비검색 by 2ono</a>
+            </div>
+         </div>
+        `;
+
+
+        $('#movie').html(output);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 }
